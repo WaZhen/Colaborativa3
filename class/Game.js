b36ca35fd1b0da286096428797c471e9e496c549
WaZhen <wangzhenou@gmail.com>
2017-05-03 12:56:34 +0000

    corregido angulos de camara permitidos

--- class/Game.js
+++ class/Game.js
@@ -9,18 +9,20 @@ var Game = function(argContainer) {
     this.width = window.innerWidth;
     this.height = window.innerHeight
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 1, 5000);
-    this.camera.position.set(10, 10, 10)
-    this.scene.add(this.camera);
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setClearColor(0xffffff);
     this.renderer.setSize(this.width, this.height);
+    
+    
+    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 1, 7000);
+    this.camera.position.set(10, 10, 10)
+    this.scene.add(this.camera);
+   
     argContainer.appendChild(this.renderer.domElement);
-    
-    this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
-    
+   
     this.background = new Background(gameScope.scene);
+    this.orbit = new OrbitControls(this.camera, this.renderer.domElement, this.background);
     
     this.planets = [];
     
--- class/OrbitControls.js
+++ class/OrbitControls.js
@@ -1,7 +1,10 @@
-var OrbitControls = function(camera, renderer) {
+var OrbitControls = function(camera, renderer, background) {
     this.controls = new THREE.OrbitControls(camera, renderer);
     this.controls.enableZoom = true;
 
-    this.controls.minZoom = 1;
-    this.controls.maxZoom = 1000;
+    this.controls.minDistance = 1;
+    this.controls.maxDistance = 1000;
+    
+    this.controls.maxPolarAngle = 2.3;
+    this.controls.minPolarAngle = 1;
 }
